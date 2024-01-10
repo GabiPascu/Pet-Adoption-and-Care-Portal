74afda169a8e2212c6e34a2c5d0ba3c441f624ab
GabiPascu <pop_gabi00@yahoo.com>
2024-01-10 14:27:45 +0200

    first commit

---
+++ src/main/java/com/Pet/Adoption/and/Care/Portal/PetAdoptionAndCarePortalApplication.java
@@ -0,0 +1,13 @@
+package com.Pet.Adoption.and.Care.Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionAndCarePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionAndCarePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pet/Adoption/and/Care/Portal/PetAdoptionAndCarePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pet.Adoption.and.Care.Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionAndCarePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
